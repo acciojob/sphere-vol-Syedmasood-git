18f98aa8e3554a2c7f625c8821ee0f657327190d
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-21 23:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
-    //Write your code here
-  
+	let r=document.getElementById("radius").value;
+	// r=Math.abs(r);
+	let volume=(4/3)*Math.PI*Math.pow(r,3);
+	// volume=volume.toFixed(4);
+	document.getElementById("volume").value=volume;
+	// volume.innerText=formula;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
