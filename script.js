4aaf284236cd2e7d19c380a1b08b45cdb075abf2
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-21 23:32:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
 	let r=document.getElementById("radius").value;
 	// r=Math.abs(r);
 	let volume=(4/3)*Math.PI*Math.pow(r,3);
-	// volume=volume.toFixed(4);
+	volume=volume.toFixed(4);
 	document.getElementById("volume").value=volume;
 	// volume.innerText=formula;
 	return false;
